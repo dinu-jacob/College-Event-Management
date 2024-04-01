18e4296ab0b5440b8e7978e924c3c29db52838a4
dinu_jacob <dinu.jacob@maxwellgeosystems.com>
2024-04-01 10:58:22 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,107 @@
+
+<html>
+<head>
+<title> Online Event Management</title>
+
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+
+
+</head>
+
+<body>
+
+
+ 
+<div class="maindiv"> 
+
+<div class="logo1"><img src="images/logo.png"> 
+</div>
+
+
+<div class="menu">
+<h1>COLLEGE EVENT MANAGMENT<h1>
+</div>
+<div class="slider">
+<div >
+
+<style>
+.mySlides {display:none;}
+</style>
+<body>
+
+<div class="slider"  >
+  <div class="mySlides "><img src="images/slider-1.png" /></div>
+    <div class="mySlides "><img src="images/dj.png" /></div>
+<div class="mySlides "><img src="images/mime.png" /></div>
+  <div class="mySlides "><img src="images/slider.png" /> </div>
+   <div class="mySlides "><img src="images/bha.png" /></div>
+  </div>
+
+<script>
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1} 
+  x[slideIndex-1].style.display = "block"; 
+  setTimeout(carousel, 2000); 
+}
+</script>
+</div>
+</div>
+
+
+<div> 
+<center>
+
+<div class="wrapper">
+  <div class="link_wrapper">
+    <div class="icon">
+      
+    </div>
+  </div>
+  
+</div>
+
+
+<div class=" bottombox-1" align="center">
+ 
+  <A href="adminlogin.php" style="text-decoration:none" > <h4>Admin Login</h4> </A>
+  
+</div>
+<div class=" bottombox-1"  align="center">
+  
+   
+    
+       <A href="studentlogin.php" style="text-decoration:none"> <h4>Student Login</h4> </A>
+  </center>
+</div>
+
+	<div class="clear"></div>
+	
+  
+     
+      <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3932.9308918106713!2d76.6379806142802!3d9.686939681005219!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b07d332199cbaeb%3A0x59a721c86e68bcb7!2sBishop%20Vayalil%20Memorial%20Holy%20Cross%20College%20Cherpunkal!5e0!3m2!1sen!2sin!4v1627044403285!5m2!1sen!2sin" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy"></iframe></p>
+  
+	  <div class="clear"></div>
+	  	
+	<div class="footer-1">
+<p>Copyright © 2021 . All Rights Reserved. Designed & Developed by Dinu and Christy</p>
+</div>
+
+    </div>
+
+
+  </div>
+  
+
+
+
+</body>
+</html>
